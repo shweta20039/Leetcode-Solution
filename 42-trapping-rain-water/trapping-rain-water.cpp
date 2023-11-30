fa11adb2963cde1d946a4206ca6d7376c347c4aa
shweta20039 <126647760+shweta20039@users.noreply.github.com>
2023-11-30 17:04:51 +0530

    Time: 10 ms (71.47%) | Memory: 20 MB (96.04%) - LeetSync

---
+++ 42-trapping-rain-water/trapping-rain-water.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int trap(vector<int>& h) {
+
+
+                int count=0;
+            int l=0,r=h.size()-1;
+            int lmax=INT_MIN;
+            int rmax=INT_MIN;
+            while(l<r)
+            {
+                lmax=max(lmax,h[l]);
+                rmax=max(rmax,h[r]);
+                count+=(lmax<rmax)?lmax-h[l++]:rmax-h[r--];
+            }
+            return count;
+    }
+};
