b5cdd8ddcc18e804d96c11abe60365ba9d81758a
shweta20039 <126647760+shweta20039@users.noreply.github.com>
2024-02-15 12:08:51 +0530

    Time: 4 ms (96.26%) | Memory: 16 MB (32.33%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -1,41 +1,35 @@
 class Solution {
 public:
-
-//code will be in O(n+m)
     void setZeroes(vector<vector<int>>& matrix) {
-        vector<int>x,y;
-        int n=matrix.size();
-        int m=matrix[0].size();
-        for(int i=0;i<n;i++)
-    {
-        for(int j=0 ;j<m;j++)
-        {
-            if(matrix[i][j]==0)
-            {
-                x.push_back(i);
-                y.push_back(j);
-            }
-        }
-    }
-
-        for(int i=0;i<x.size();i++)
-
-        {
+        vector<int>ro,co;
+       int n=matrix.size();
+       int m=matrix[0].size();
+       for(int i=0;i<n;i++)
+       {
            for(int j=0;j<m;j++)
            {
-               matrix[x[i]][j]=0;
-           } 
-        }
+               if(matrix[i][j]==0)
+               {
+                   ro.push_back(i);
+                   co.push_back(j);
+               }
+           }
+       } 
 
-             for(int i=0;i<y.size();i++)
-
-        {
+        for(int i=0;i<ro.size();i++)
+       {
+           for(int j=0;j<m;j++)
+           {
+               matrix[ro[i]][j]=0;
+           }
+       } 
+            for(int i=0;i<co.size();i++)
+       {
            for(int j=0;j<n;j++)
            {
-               matrix[j][y[i]]=0;
-           } 
-        }
+               matrix[j][co[i]]=0;
+           }
+       } 
 
-        
     }
 };
