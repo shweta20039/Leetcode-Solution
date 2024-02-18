268fd4c5c240e59071e380164688be6a17defd0b
shweta20039 <126647760+shweta20039@users.noreply.github.com>
2024-02-19 01:14:02 +0530

    Time: 7 ms (58.82%) | Memory: 13.2 MB (5.53%) - LeetSync

---
+++ 142-linked-list-cycle-ii/linked-list-cycle-ii.cpp
@@ -0,0 +1,27 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *detectCycle(ListNode *head) {
+        
+        unordered_set<ListNode*>st;
+        ListNode*curr=head;
+        while(curr!=nullptr)
+        {
+            if(st.find(curr)!=st.end()) return curr;
+            else
+            {
+                st.insert(curr);
+                curr=curr->next;
+                
+            }
+        }
+        return nullptr;
+    }
+};
